ab73df034f5fde89b5f7448aa5f2f1d8756c8b0f
Domenico De Guglielmo <domenico.deguglielmo@domo-iot.com>
2023-04-26 09:33:25 +0200

    possible fix

--- src/mdns.rs
+++ src/mdns.rs
@@ -44,12 +44,10 @@ const ADDR_ANY: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
 #[cfg(not(target_os = "windows"))]
 fn create_socket() -> io::Result<std::net::UdpSocket> {
 
-    let addr = Ipv4Addr::new(10, 0, 1, 1);
-
     net2::UdpBuilder::new_v4()?
         .reuse_address(true)?
         .reuse_port(true)?
-        .bind((addr, MULTICAST_PORT))
+        .bind((ADDR_ANY, MULTICAST_PORT))
 }
 
 #[cfg(target_os = "windows")]
